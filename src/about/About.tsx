9ab3b13069c11b10f64a6ba4c3d112d6b1ade053
Bryan <bryandang10@gmail.com>
2024-02-08 19:59:07 -0800

    add barlow condensed

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -5,8 +5,8 @@ function About() {
 		<>
 			<Header />
 			<div className="justify-center">
-				<h1 className="text-center">About</h1>
-				<p>Mausritter is a rules-light fantasy adventure roleplaying game made by Losing Games, featuring a unique system of a physical card-based inventory system, and a heap of  home-brewed content made by the wonderful community.</p>
+				<h1 className="text-4xl font-pirata">About</h1>
+				<p className="font">Mausritter is a rules-light fantasy adventure roleplaying game made by Losing Games, featuring a unique system of a physical card-based inventory system, and a heap of  home-brewed content made by the wonderful community.</p>
 				<p>This website is a hobby project between two friends. We wanted to make a lightweight and easily accessible tool for playing Mausritter where you could experience the fun of using the card system without the physical set. We have no affiliation with Losing Games, and our project will forever remain free and open-source.</p>
 				<div>
 					<p>Bryan Dang</p>

02e3e7dd7bca13b85658213acd348ae317a83ead
Bryan <bryandang10@gmail.com>
2024-02-13 17:22:30 -0800

    add about page without images

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -9,7 +9,7 @@ interface ProfileProps {
 const Profile = ({name, imgSrc, linkedin}: ProfileProps) => (
   <div>
 		{/* <img src={imgSrc} alt={name}/> */}
-    <p className="text-xl underline font-barlow-condensed">
+    <p className="text-2xl underline font-barlow-condensed">
 			<a className="p-2 hover:bg-yellow" href={linkedin} target='blank'>{name}</a>
 		</p>
   </div>
@@ -19,15 +19,18 @@ function About() {
 	return (
 		<>
 			<Header />
-			<div className="justify-center">
-				<h1 className="text-5xl font-pirata">About</h1>
+			<div className="px-40 mx-40">
+				<h1 className="text-5xl font-pirata text-center">About</h1>
 				
-				<p className="text-xl font-barlow-condensed"><span className="font-pirata">Mausritter</span> is a rules-light fantasy adventure roleplaying game made by <span className="font-pirata">Losing Games</span>, featuring a unique system of a physical card-based inventory system, and a heap of  home-brewed content made by the wonderful community.</p>
+				<p className="text-2xl font-barlow-condensed py-4">
+					<span className="font-pirata">Mausritter</span> is a rules-light fantasy adventure roleplaying game made by <span className="font-pirata">Losing Games</span>, featuring a unique system of a physical card-based inventory system, and a heap of  home-brewed content made by the wonderful community.
+				</p>
 
-				<p className="text-xl font-barlow-condensed">This website is a hobby project between two friends. We wanted to make a lightweight and easily accessible tool for playing Mausritter where you could experience the fun of using the card system without the physical set. We have <span className="font-pirata">no affiliation with Losing Games</span>, and our project will forever remain <span className="font-pirata">free</span> and <span className="font-pirata">open-source</span>.</p>
+				<p className="text-2xl font-barlow-condensed py-4">
+					This website is a hobby project between two friends. We wanted to make a lightweight and easily accessible tool for playing Mausritter where you could experience the fun of using the card system without the physical set. We have <span className="font-pirata">no affiliation with Losing Games</span>, and our project will forever remain <span className="font-pirata">free</span> and <span className="font-pirata">open-source</span>.
+				</p>
 				
-				{/* Profiles for Bryan and Josh */}
-				<div>
+				<div className="grid grid-cols-2 gap-4 place-items-center py-4">
 					<Profile name="Bryan Dang" imgSrc="" linkedin="https://www.linkedin.com/in/bryanjhdang"/>
 					<Profile name="Joshua Li" imgSrc="" linkedin="https://www.linkedin.com/in/joshuajli/"/>
 				</div>
